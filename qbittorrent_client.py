5144826d4da421a76f24558bdbb1762914f84ebd
IvonWei <12468675@qq.com>
2020-03-24 08:38:39 +0800

    修复一处方法命名错误……

--- qbittorrent_client.py
+++ qbittorrent_client.py
@@ -9,7 +9,7 @@ from loguru import logger
 from requests import RequestException, Session
 
 logger = logger.bind(name='qbittorrent_client')
-__version__ = 'v0.3.5'
+__version__ = 'v0.3.6'
 
 
 def singleton(cls):
