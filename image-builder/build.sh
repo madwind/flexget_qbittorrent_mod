5c319521152dd56bbe47ea0c9e203972509fbe75
IvonWei <12468675@qq.com>
2020-05-30 10:58:35 +0800

    flexget dockerfile

---
+++ image-builder/build.sh
@@ -0,0 +1,2 @@
+set -x
+docker build --no-cache -t ivonwei/flexget $(dirname $0)
---
+++ image-builder/rootfs/usr/bin/entrypoint.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+# entrypoint.sh
+
+cd ~/.flexget/config
+
+if [ ! -f ~/.flexget/config/config.yml ]; then
+  echo "$(date '+%Y-%m-%d %H:%m') INIT     New config.yml from template"
+  cp /etc/flexget/config.example.yml ~/.flexget/config/config.yml
+fi
+
+if [ ! -z "${FG_WEBUI_PASSWD}" ]; then
+  echo "$(date '+%Y-%m-%d %H:%m') INIT     Using userdefined FG_WEBUI_PASSWD: ${FG_WEBUI_PASSWD}"
+  flexget web passwd "${FG_WEBUI_PASSWD}"
+fi
+
+flexget -L ${FG_LOG_LEVEL:-info} daemon start --autoreload-config
