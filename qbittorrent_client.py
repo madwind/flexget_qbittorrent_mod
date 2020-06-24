afd0e2a54f50374b210ce8401fa5c5a5a3b9d5aa
IvonWei <12468675@qq.com>
2020-06-24 19:38:03 +0800

    v0.5.1

--- qbittorrent_client.py
+++ qbittorrent_client.py
@@ -9,7 +9,7 @@ from loguru import logger
 from requests import RequestException, Session
 
 logger = logger.bind(name='qbittorrent_client')
-__version__ = 'v0.5'
+__version__ = 'v0.5.1'
 
 
 def singleton(cls):
