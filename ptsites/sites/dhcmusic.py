c8a212a75044e25f4b95b458b68380fe07e0fe58
IvonWei <12468675@qq.com>
2020-07-19 19:13:38 +0800

    dhcmusic 适配

---
+++ ptsites/sites/dhcmusic.py
@@ -0,0 +1,13 @@
+from flexget import plugin
+
+from ..executor import Executor
+
+# auto_sign_in
+URL = 'http://dhcmusic.xyz/attendance.php'
+SUCCEED_REGEX = '这是您的第 .* 次签到，已连续签到 .* 天，本次签到获得 .* 个魔力值。|您今天已经签到过了，请勿重复刷新。'
+
+
+class MainClass(Executor):
+    @staticmethod
+    def build_sign_in_entry(entry, site_name, config):
+        Executor.build_sign_in_entry_common(entry, site_name, config, URL, SUCCEED_REGEX)
