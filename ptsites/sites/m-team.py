30f270608f478111c80ae73f68c986773a316b46
IvonWei <12468675@qq.com>
2020-07-29 17:38:19 +0800

    签到支持 m-team 二次验证

--- ptsites/sites/m-team.py
+++ ptsites/sites/m-team.py
@@ -1,7 +1,11 @@
-from ..executor import Executor
+import re
+
+from ..utils.google_auth import GoogleAuth
+from ..executor import Executor, SignState
 
 # auto_sign_in
 URL = 'https://pt.m-team.cc/'
+VERIFY_URL = 'https://pt.m-team.cc/verify.php'
 SUCCEED_REGEX = '歡迎回來'
 MESSAGE_URL = 'https://pt.m-team.cc/messages.php?action=viewmailbox&box=-2'
 
@@ -9,9 +13,30 @@ MESSAGE_URL = 'https://pt.m-team.cc/messages.php?action=viewmailbox&box=-2'
 class MainClass(Executor):
     @staticmethod
     def build_sign_in_entry(entry, site_name, config):
-        Executor.build_sign_in_entry_common(entry, site_name, config, URL, SUCCEED_REGEX)
+        if isinstance(entry['site_config'], str):
+            Executor.build_sign_in_entry_common(entry, site_name, config, URL, SUCCEED_REGEX)
+        else:
+            entry['secret_key'] = entry['site_config']['secret_key']
+            entry['site_config'] = entry['site_config']['cookie']
+            Executor.build_sign_in_entry_common(entry, site_name, config, URL, SUCCEED_REGEX)
 
     @staticmethod
     def build_reseed_entry(entry, base_url, site, passkey, torrent_id):
         download_page = site['download_page'].format(torrent_id=torrent_id, passkey=passkey)
         entry['url'] = 'https://{}/{}&https=1'.format(base_url, download_page)
+
+    def sign_in_by_get(self, entry, config):
+        response = self._request(entry, 'get', entry['url'], headers=entry['headers'])
+        net_state = self.check_net_state(entry, response, entry['url'])
+        if net_state == SignState.URL_REDIRECT:
+            if response.url.startswith(VERIFY_URL):
+                content = self._decode(response)
+                attempts = re.search('您還有30次嘗試機會，否則該IP將被禁止訪問。', content)
+                if attempts:
+                    google_code = GoogleAuth.calc(entry['secret_key'])
+                    data = {
+                        'otp': (None, google_code)
+                    }
+                    response = self._request(entry, 'post', VERIFY_URL, files=data)
+
+        self.final_check(entry, response, entry['url'])
---
+++ ptsites/utils/google_auth.py
@@ -0,0 +1,19 @@
+import hmac, base64, struct, hashlib, time
+import sys
+
+
+class GoogleAuth:
+    @staticmethod
+    def calc(secret_key):
+        input_time = int(time.time()) // 30
+        key = base64.b32decode(secret_key)
+        msg = struct.pack(">Q", input_time)
+        google_code = hmac.new(key, msg, hashlib.sha1).digest()
+        if sys.version_info > (2, 7):
+            o = google_code[19] & 15
+        else:
+            o = ord(google_code[19]) & 15
+        google_code = str((struct.unpack(">I", google_code[o:o + 4])[0] & 0x7fffffff) % 1000000)
+        if len(google_code) == 5:
+            google_code = '0' + google_code
+        return google_code
