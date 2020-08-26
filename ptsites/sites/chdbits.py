f152eb4a5a63eba9d628623c14151f0f63216284
IvonWei <12468675@qq.com>
2020-08-26 15:00:52 +0800

    chdbits 适配

---
+++ ptsites/sites/chdbits.py
@@ -0,0 +1,11 @@
+from ..nexusphp import NexusPHP
+
+# auto_sign_in
+URL = 'https://chdbits.co/'
+SUCCEED_REGEX = '欢迎回来'
+
+
+class MainClass(NexusPHP):
+    @staticmethod
+    def build_sign_in_entry(entry, site_name, config):
+        NexusPHP.build_sign_in_entry(entry, site_name, config, URL, SUCCEED_REGEX)
