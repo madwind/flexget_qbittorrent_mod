cfc7aac865221a011c3c51c339f14dcdfcf4a5a7
IvonWei <12468675@qq.com>
2020-08-27 09:50:46 +0800

    chdbits 答题签到

--- ptsites/sites/chdbits.py
+++ ptsites/sites/chdbits.py
@@ -1,11 +1,15 @@
 from ..nexusphp import NexusPHP
 
 # auto_sign_in
-URL = 'https://chdbits.co/'
-SUCCEED_REGEX = '欢迎回来'
+URL = 'https://chdbits.co/bakatest.php'
+SUCCEED_REGEX = '签到成功,获得\\d+点魔力值|今天已经签过到了\\(已连续\\d+天签到\\)'
+WRONG_REGEX = '回答错误,失去 1 魔力值,这道题还会再考一次'
 
 
 class MainClass(NexusPHP):
     @staticmethod
     def build_sign_in_entry(entry, site_name, config):
         NexusPHP.build_sign_in_entry(entry, site_name, config, URL, SUCCEED_REGEX)
+
+    def sign_in(self, entry, config):
+        self.sign_in_by_question(entry, config)
