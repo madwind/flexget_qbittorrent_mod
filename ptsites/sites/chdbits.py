d117c4aecd25a9ce42721fa9ddf348891ceda78b
IvonWei <12468675@qq.com>
2020-08-27 09:55:05 +0800

    chdbits 答题签到

--- ptsites/sites/chdbits.py
+++ ptsites/sites/chdbits.py
@@ -9,7 +9,7 @@ WRONG_REGEX = '回答错误,失去 1 魔力值,这道题还会再考一次'
 class MainClass(NexusPHP):
     @staticmethod
     def build_sign_in_entry(entry, site_name, config):
-        NexusPHP.build_sign_in_entry(entry, site_name, config, URL, SUCCEED_REGEX)
+        NexusPHP.build_sign_in_entry(entry, site_name, config, URL, SUCCEED_REGEX, wrong_regex=WRONG_REGEX)
 
     def sign_in(self, entry, config):
         self.sign_in_by_question(entry, config)
