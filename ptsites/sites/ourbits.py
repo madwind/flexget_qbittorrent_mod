9e938508c2f00a929f8662fa27fc552bd8a4119a
IvonWei <12468675@qq.com>
2020-09-08 20:59:42 +0800

    ourbits 模拟登陆

--- ptsites/sites/ourbits.py
+++ ptsites/sites/ourbits.py
@@ -2,14 +2,27 @@ from ..site_base import SiteBase
 from ..nexusphp import NexusPHP
 
 # auto_sign_in
+LOGIN_URL = 'https://ourbits.club/takelogin.php'
+LOGIN_SUCCEED_URL = 'https://ourbits.club/index.php'
 URL = 'https://ourbits.club/attendance.php'
 SUCCEED_REGEX = '这是您的第 .* 次签到，已连续签到 .* 天，本次签到获得 .* 个魔力值。|您今天已经签到过了，请勿重复刷新。'
 
 
+# site_config
+# login:
+#    username: 'xxxxx'
+#    password: 'xxxxxxxx'
+
 class MainClass(NexusPHP):
     @staticmethod
     def build_sign_in(entry, config):
-        SiteBase.build_sign_in_entry(entry, config, URL, SUCCEED_REGEX)
+        entry['url'] = URL
+        entry['succeed_regex'] = SUCCEED_REGEX
+        headers = {
+            'user-agent': config.get('user-agent'),
+            'referer': URL
+        }
+        entry['headers'] = headers
 
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
@@ -18,3 +31,15 @@ class MainClass(NexusPHP):
             'group': 2,
         }
         return selector
+
+    def sign_in(self, entry, config):
+        login = entry['site_config'].get('login')
+        login['2fa_code'] = ''
+        login['trackerssl'] = 'yes'
+        login_response = self._request(entry, 'post', LOGIN_URL, data=login)
+        login_state = self.check_net_state(entry, login_response, LOGIN_SUCCEED_URL)
+        if login_state:
+            entry['result'] = 'login failed'
+            entry.fail(entry['result'])
+        else:
+            self.sign_in_by_get(entry, config)
