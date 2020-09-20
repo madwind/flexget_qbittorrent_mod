b1cd691cbb1cd39f3d50af036d6a7e4279af0ad0
IvonWei <12468675@qq.com>
2020-09-21 00:10:12 +0800

    用户名密码登陆

--- ptsites/sites/m-team.py
+++ ptsites/sites/m-team.py
@@ -1,13 +1,15 @@
 import re
 
+from loguru import logger
+
+from ..nexusphp import NexusPHP
 from ..site_base import SignState
 from ..utils.google_auth import GoogleAuth
-from ..site_base import SiteBase
-from ..nexusphp import NexusPHP
 
 # auto_sign_in
-URL = 'https://pt.m-team.cc/'
-VERIFY_URL = 'https://pt.m-team.cc/verify.php?returnto=%2F'
+LOGIN_URL = 'https://pt.m-team.cc/takelogin.php'
+URL = 'https://pt.m-team.cc/index.php'
+VERIFY_URL = 'https://pt.m-team.cc/verify.php?returnto='
 SUCCEED_REGEX = '歡迎回來'
 SYSTEM_MESSAGE_URL = '/messages.php?action=viewmailbox&box=-2'
 
@@ -15,12 +17,13 @@ SYSTEM_MESSAGE_URL = '/messages.php?action=viewmailbox&box=-2'
 class MainClass(NexusPHP):
     @staticmethod
     def build_sign_in(entry, config):
-        if isinstance(entry['site_config'], str):
-            SiteBase.build_sign_in_entry(entry, config, URL, SUCCEED_REGEX)
-        else:
-            entry['secret_key'] = entry['site_config']['secret_key']
-            entry['site_config'] = entry['site_config']['cookie']
-            SiteBase.build_sign_in_entry(entry, config, URL, SUCCEED_REGEX)
+        entry['url'] = URL
+        entry['succeed_regex'] = SUCCEED_REGEX
+        headers = {
+            'user-agent': config.get('user-agent'),
+            'referer': URL
+        }
+        entry['headers'] = headers
 
     @staticmethod
     def build_reseed_entry(entry, base_url, site, passkey, torrent_id):
@@ -28,20 +31,39 @@ class MainClass(NexusPHP):
         entry['url'] = 'https://{}/{}&https=1'.format(base_url, download_page)
 
     def sign_in_by_get(self, entry, config):
-        entry['base_response'] = response = self._request(entry, 'get', entry['url'])
-        net_state = self.check_net_state(entry, response, entry['url'])
+        login = entry['site_config'].get('login')
+        if not login:
+            entry.fail('Login data not found!')
+            return
+
+        data = {
+            'username': login['username'],
+            'password': login['password'],
+        }
+
+        response = self._request(entry, 'post', LOGIN_URL, data=data)
+
+        login_state = self.check_net_state(entry, response, URL)
+        if login_state:
+            entry.fail('Login failed!')
+
         use_google_auth = False
-        if net_state == SignState.URL_REDIRECT:
-            if response.url.startswith(VERIFY_URL):
-                content = self._decode(response)
-                attempts = re.search('您還有30次嘗試機會，否則該IP將被禁止訪問。', content)
-                if attempts:
-                    google_code = GoogleAuth.calc(entry['secret_key'])
+        if response.url.startswith(VERIFY_URL):
+            content = self._decode(response)
+            attempts = re.search('您還有(\\d+)次嘗試機會，否則該IP將被禁止訪問。', content)
+            if attempts:
+                times = attempts.group(1)
+                if times == '30':
+                    google_code = GoogleAuth.calc(login['secret_key'])
                     data = {
                         'otp': (None, google_code)
                     }
                     entry['base_response'] = response = self._request(entry, 'post', VERIFY_URL, files=data)
-
+                    use_google_auth = True
+                else:
+                    entry.fail('{} with google_auth'.format(attempts.group()))
+            else:
+                entry.fail('Attempts text not found!  with google_auth')
         self.final_check(entry, response, entry['url'])
         if use_google_auth:
             entry['result'] = entry['result'] + ' with google_auth'
