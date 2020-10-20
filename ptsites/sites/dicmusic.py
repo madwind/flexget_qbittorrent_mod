dfbaaf8f68c3802b165ecd57f71f42b311778456
IvonWei <12468675@qq.com>
2020-10-20 10:01:16 +0800

    适配 gazellegames

--- ptsites/sites/dicmusic.py
+++ ptsites/sites/dicmusic.py
@@ -1,5 +1,5 @@
-from ..schema.site_base import SiteBase
 from ..schema.gazelle import Gazelle
+from ..schema.site_base import SiteBase
 
 # auto_sign_in
 
@@ -27,6 +27,14 @@ class MainClass(Gazelle):
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
         self.dict_merge(selector, {
+            'detail_sources': {
+                'default': {
+                    'elements': {'table': '#content > div > div.sidebar > div.box.box_info.box_userinfo_stats > ul'}
+                },
+                'extend': {
+                    'link': '/ajax.php?action=community_stats&userid={}'
+                }
+            },
             'details': {
                 'hr': None
             }
