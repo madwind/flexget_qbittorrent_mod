fb9600de3f24304d3983e95213ef34786ab4318a
IvonWei <12468675@qq.com>
2020-10-25 17:56:59 +0800

    1468适配

---
+++ ptsites/sites/1468.py
@@ -0,0 +1,12 @@
+from ..schema.nexusphp import NexusPHP
+from ..schema.site_base import SiteBase
+
+# auto_sign_in
+URL = 'https://www.1468.top/attendance.php'
+SUCCEED_REGEX = '这是您的第 .* 次签到，已连续签到 .* 天，本次签到获得 .* 个魔力值。|您今天已经签到过了，请勿重复刷新。'
+
+
+class MainClass(NexusPHP):
+    @staticmethod
+    def build_sign_in(entry, config):
+        SiteBase.build_sign_in_entry(entry, config, URL, SUCCEED_REGEX)
