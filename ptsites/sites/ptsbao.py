7e10d129b04a1dbdf878b627270bde97dbff61eb
IvonWei <12468675@qq.com>
2020-12-13 10:16:41 +0800

    points

--- ptsites/sites/ptsbao.py
+++ ptsites/sites/ptsbao.py
@@ -10,3 +10,14 @@ class MainClass(NexusPHP):
     @staticmethod
     def build_sign_in(entry, config):
         SiteBase.build_sign_in_entry(entry, config, URL, SUCCEED_REGEX)
+
+    def build_selector(self):
+        selector = super(MainClass, self).build_selector()
+        self.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': '魔力值.*?：([\\d,.]+)'
+                }
+            }
+        })
+        return selector
