481d82d2f9f93af4d04482c96fb4bdd61b384458
IvonWei <12468675@qq.com>
2020-12-14 20:43:16 +0800

    leaguehd -> lemonhd

--- ptsites/sites/lemonhd.py
+++ ptsites/sites/lemonhd.py
@@ -2,7 +2,7 @@ from ..schema.site_base import SiteBase
 from ..schema.nexusphp import NexusPHP
 
 # auto_sign_in
-URL = 'https://leaguehd.com/attendance.php'
+URL = 'https://lemonhd.org/attendance.php'
 SUCCEED_REGEX = '这是您的第 .* 次签到，已连续签到 .* 天，本次签到获得 .* 魔力值。|您今天已经签到过了，请勿重复刷新。'
 
 
