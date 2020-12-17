dd5351cad48021c7f39fbed0fb3100452337b0eb
IvonWei <12468675@qq.com>
2020-12-17 09:29:22 +0800

    1468 -> htpt

--- ptsites/sites/htpt.py
+++ ptsites/sites/htpt.py
@@ -2,7 +2,7 @@ from ..schema.nexusphp import NexusPHP
 from ..schema.site_base import SiteBase
 
 # auto_sign_in
-URL = 'https://www.1468.top/attendance.php'
+URL = 'https://www.htpt.top/attendance.php'
 SUCCEED_REGEX = '这是您的第 .* 次签到，已连续签到 .* 天，本次签到获得 .* 个魔力值。|您今天已经签到过了，请勿重复刷新。'
 
 
