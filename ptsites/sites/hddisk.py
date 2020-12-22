93ca80607294a47fe4b4c406d60df1ac8fe3baf7
IvonWei <12468675@qq.com>
2020-12-22 10:40:17 +0800

    USER_CLASSES

--- ptsites/sites/hddisk.py
+++ ptsites/sites/hddisk.py
@@ -3,3 +3,8 @@ from ..schema.nexusphp import AttendanceHR
 
 class MainClass(AttendanceHR):
     URL = 'https://hddisk.life/'
+    USER_CLASSES = {
+        'downloaded': [805306368000, 3298534883328],
+        'share_ratio': [3.05, 4.55],
+        'days': [280, 700]
+    }
