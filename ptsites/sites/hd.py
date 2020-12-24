77076d8ec28f8a9f7ca6578c74a34c9ca5ad546d
IvonWei <12468675@qq.com>
2020-12-24 12:50:02 +0800

    SUCCEED_REGEX

--- ptsites/sites/hd.py
+++ ptsites/sites/hd.py
@@ -3,6 +3,7 @@ from ..schema.nexusphp import Visit
 
 class MainClass(Visit):
     URL = 'https://www.hd.ai/'
+    SUCCEED_REGEX = '(?<=<i class="layui-icon layui-icon-username">)</i>.*?(?=</a>)'
     USER_CLASSES = {
         'downloaded': [805306368000, 3298534883328],
         'share_ratio': [3.05, 4.55],
