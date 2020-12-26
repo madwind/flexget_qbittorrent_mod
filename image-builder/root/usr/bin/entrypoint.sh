34925bcef79bbcb160368530fc3fbea89e65076b
IvonWei <12468675@qq.com>
2020-12-26 20:36:07 +0800

    python 3 & flexget 3.1.91

--- image-builder/root/usr/bin/entrypoint.sh
+++ image-builder/root/usr/bin/entrypoint.sh
@@ -2,40 +2,39 @@
 #
 # entrypoint.sh
 
-# make folders
-mkdir -p /home/docker
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+
+groupmod -o -g "$PGID" flexget
+usermod -o -u "$PUID" flexget
 
 # remove config-lock
-[[ -f /config/.config-lock ]] && \
-  { echo "Removing lockfile"; rm -f /config/.config-lock; }
-
-# copy config.yml
-[[ -f /config/config.yml ]] && \
-  { echo "Using existing config.yml"; } || \
-	{ echo "New config.yml from template"; cp /defaults/config.example.yml /config/config.yml; }
-
-# set FG_WEBUI_PASSWD
-if [[ ! -z "${FG_WEBUI_PASSWD}" ]]; then
-  echo "Setting flexget web password to '${FG_WEBUI_PASSWD}'"
-  flexget -c /config/config.yml web passwd "${FG_WEBUI_PASSWD}" | grep 'Updated password' >/dev/null 2>&1 && \
-    echo "Updated password" || \
-    echo "Oops, something went wrong"
+if [ -f "/config/.config-lock" ]; then
+  echo "Removing lockfile"
+  rm -f /config/.config-lock
 fi
 
-echo docker:x:${PUID:-0}:${PGID:-0}:docker:/home/docker:/bin/ash >> /etc/passwd
-echo docker:x:${PGID:-0}: >> /etc/group
+# copy config.yml
+if [ -f "/config/config.yml" ]; then
+  echo "Using existing config.yml"
+else
+  echo "New config.yml from template"
+  cp /defaults/config.example.yml /config/config.yml
+fi
+
+# set FG_WEBUI_PASSWD
+if [ ! -z "${FG_WEBUI_PASSWD}" ]; then
+  echo "Setting flexget web password to '${FG_WEBUI_PASSWD}'"
+  flexget -c /config/config.yml web passwd "${FG_WEBUI_PASSWD}" | grep 'Updated password' >/dev/null 2>&1 && \
+  echo "Updated password" || \
+  echo "Oops, something went wrong"
+fi
+
 
 # permissions
-chown -R docker:docker /config
-chown docker:docker /downloads
-chown docker:docker /home/docker
-
-chmod 775 \
-	/config \
-	/downloads \
-  /home/docker
+chown -R flexget:flexget /config
+chown -R flexget:flexget /downloads
+chown -R flexget:flexget /home/flexget
 
 
-su docker -c "python -c \"import os;from pyppeteer import chromium_downloader;folder=f'{chromium_downloader.DOWNLOADS_FOLDER}/{chromium_downloader.REVISION}/chrome-linux';os.makedirs(folder);os.symlink('/usr/bin/chromium-browser',f'{chromium_downloader.DOWNLOADS_FOLDER}/{chromium_downloader.REVISION}/chrome-linux/chrome')\""
-
-su docker -c "flexget -c /config/config.yml -L ${FG_LOG_LEVEL:-info} daemon start --autoreload-config"
+su flexget -c "/usr/local/bin/flexget -c /config/config.yml -L ${FG_LOG_LEVEL:-info} daemon start --autoreload-config"
