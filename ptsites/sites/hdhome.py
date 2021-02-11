efe832a782d3a998813073e7302033d5ee5103e5
IvonWei <12468675@qq.com>
2021-02-11 13:38:23 +0800

    reseed: 修复hdhome

--- ptsites/sites/hdhome.py
+++ ptsites/sites/hdhome.py
@@ -1,4 +1,5 @@
 from ..schema.nexusphp import AttendanceHR
+from ..schema.site_base import SiteBase
 
 
 class MainClass(AttendanceHR):
@@ -8,3 +9,8 @@ class MainClass(AttendanceHR):
         'share_ratio': [5.5],
         'days': [70]
     }
+
+    @classmethod
+    def build_reseed(cls, entry, config, site, passkey, torrent_id):
+        SiteBase.build_reseed_from_page(entry, config, passkey, torrent_id, cls.URL, cls.TORRENT_PAGE_URL,
+                                        '/download\\.php\\?id=\\d+&downhash=.+?(?=")')
