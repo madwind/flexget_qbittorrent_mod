ac5de665ca6bbce8b82bfdff0230fd1498c3267e
IvonWei <12468675@qq.com>
2021-02-11 12:45:49 +0800

    reseed: 修复hddolby pthome

--- ptsites/sites/pthome.py
+++ ptsites/sites/pthome.py
@@ -1,5 +1,5 @@
 from ..schema.nexusphp import AttendanceHR
-
+from ..schema.site_base import SiteBase
 
 
 class MainClass(AttendanceHR):
@@ -9,3 +9,8 @@ class MainClass(AttendanceHR):
         'share_ratio': [6, 9],
         'days': [280, 700]
     }
+
+    @classmethod
+    def build_reseed(cls, entry, config, site, passkey, torrent_id):
+        SiteBase.build_reseed_from_page(entry, config, passkey, torrent_id, cls.URL, cls.TORRENT_PAGE_URL,
+                                        '/download\\.php\\?id=\\d+&downhash=.+?(?=")')
