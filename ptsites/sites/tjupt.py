677bdc8f2c3ac3b150ab3d3510e2f9dc32d3de31
IvonWei <12468675@qq.com>
2021-02-19 14:03:33 +0800

    tjupt hr

--- ptsites/sites/tjupt.py
+++ ptsites/sites/tjupt.py
@@ -1,10 +1,20 @@
-from ..schema.nexusphp import Visit
+from ..schema.nexusphp import VisitHR
+from ..utils.net_utils import NetUtils
 
 
-class MainClass(Visit):
+class MainClass(VisitHR):
     URL = 'https://tjupt.org/'
     USER_CLASSES = {
         'downloaded': [805306368000, 3298534883328],
         'share_ratio': [3.05, 4.55],
         'days': [280, 700]
     }
+
+    def build_selector(self):
+        selector = super(VisitHR, self).build_selector()
+        NetUtils.dict_merge(selector, {
+            'details': {
+                'hr': 'HnR积分.*?(\\d+)'
+            }
+        })
+        return selector
