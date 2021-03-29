86bb7708fc3969919166491482b17f474a6b5d92
IvonWei <12468675@qq.com>
2021-03-29 11:53:56 +0800

    做种积分

--- ptsites/sites/hdhome.py
+++ ptsites/sites/hdhome.py
@@ -1,5 +1,6 @@
 from ..schema.nexusphp import AttendanceHR
 from ..schema.site_base import SiteBase
+from ..utils.net_utils import NetUtils
 
 
 class MainClass(AttendanceHR):
@@ -7,6 +8,7 @@ class MainClass(AttendanceHR):
     USER_CLASSES = {
         'downloaded': [8796093022208],
         'share_ratio': [5.5],
+        'ponits': [1000000],
         'days': [70]
     }
 
@@ -14,3 +16,14 @@ class MainClass(AttendanceHR):
     def build_reseed(cls, entry, config, site, passkey, torrent_id):
         SiteBase.build_reseed_from_page(entry, config, passkey, torrent_id, cls.URL, cls.TORRENT_PAGE_URL,
                                         '/download\\.php\\?id=\\d+&downhash=.+?(?=")')
+
+    def build_selector(self):
+        selector = super(MainClass, self).build_selector()
+        NetUtils.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': '做种积分([\\d.,]+)',
+                }
+            }
+        })
+        return selector
