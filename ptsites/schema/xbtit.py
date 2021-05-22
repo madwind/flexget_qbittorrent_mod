d66b904542126e9d4fc99c7e0f2acb8de923725f
IvonWei <12468675@qq.com>
2021-05-22 15:29:29 +0800

    xbtit

---
+++ ptsites/schema/xbtit.py
@@ -0,0 +1,116 @@
+import re
+from urllib.parse import urljoin
+
+from dateutil.parser import parse
+from flexget.utils.soup import get_soup
+
+from ..schema.site_base import SiteBase, Work, SignState, NetworkState
+from ..utils.net_utils import NetUtils
+
+
+class XBTIT(SiteBase):
+    SUCCEED_REGEX = None
+    USER_CLASSES = {
+        'uploaded': [8796093022208],
+        'share_ratio': [5.5],
+        'days': [70]
+    }
+
+    @classmethod
+    def build_workflow(cls):
+        return [
+            Work(
+                url='/',
+                method='get',
+                succeed_regex=cls.SUCCEED_REGEX,
+                fail_regex=None,
+                check_state=('final', SignState.SUCCEED),
+                is_base_content=True
+            )
+        ]
+
+    def build_selector(self):
+        selector = {
+            'user_id': 'usercp.php\\?uid=(\\d+)',
+            'detail_sources': {
+                'default': {
+                    'link': '/usercp.php?uid={}',
+                    'elements': {
+                        'bar': 'body > div.mainmenu > table:nth-child(5)',
+                        'table': '#CurrentDetailsHideShowTR'
+                    }
+                }
+            },
+            'details': {
+                'uploaded': {
+                    'regex': '↑.([\\d.]+ [ZEPTGMK]?iB)'
+                },
+                'downloaded': {
+                    'regex': '↓.([\\d.]+ [ZEPTGMK]?iB)'
+                },
+                'share_ratio': {
+                    'regex': 'Ratio: ([\\d.]+)'
+                },
+                'points': {
+                    'regex': 'Bonus Points:.+?([\\d,.]+)'
+                },
+                'join_date': {
+                    'regex': 'Joined on.*?(\\d{2}/\\d{2}/\\d{4})',
+                    'handle': self.handle_join_date
+                },
+                'seeding': {
+                    'regex': 'Seeding (\\d+)'
+                },
+                'leeching': {
+                    'regex': 'Leeching (\\d+)'
+                },
+                'hr': None
+            }
+        }
+        return selector
+
+    def get_XBTIT_message(self, entry, config, MESSAGES_URL_REGEX='usercp\\.php\\?uid=\\d+&do=pm&action=list'):
+        if messages_url_match := re.search(MESSAGES_URL_REGEX, entry['base_content']):
+            messages_url = messages_url_match.group()
+        else:
+            entry.fail_with_prefix('Can not found messages_url.')
+            return
+        messages_url = urljoin(entry['url'], messages_url)
+        message_box_response = self._request(entry, 'get', messages_url)
+        network_state = self.check_network_state(entry, messages_url, message_box_response)
+        if network_state != NetworkState.SUCCEED:
+            entry.fail_with_prefix('Can not read message box! url:{}'.format(messages_url))
+            return
+
+        message_elements = get_soup(NetUtils.decode(message_box_response)).select(
+            'tr > td.lista:nth-child(1)')
+        unread_elements = filter(lambda elements: elements.get_text() == 'no', message_elements)
+        failed = False
+        for unread_element in unread_elements:
+            td = unread_element.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
+            title = td.text
+            href = td.a.get('href')
+            messages_url = urljoin(messages_url, href)
+            message_response = self._request(entry, 'get', messages_url)
+            network_state = self.check_network_state(entry, [messages_url], message_response)
+            if network_state != NetworkState.SUCCEED:
+                message_body = 'Can not read message body!'
+                failed = True
+            else:
+                body_element = get_soup(NetUtils.decode(message_response)).select_one(
+                    '#PrivateMessageHideShowTR > td > table:nth-child(1) > tbody > tr:nth-child(2) > td')
+                if body_element:
+                    message_body = body_element.text.strip()
+            entry['messages'] = entry['messages'] + (
+                '\nTitle: {}\nLink: {}\n{}'.format(title, messages_url, message_body))
+        if failed:
+            entry.fail_with_prefix('Can not read message body!')
+
+    def get_message(self, entry, config):
+        self.get_XBTIT_message(entry, config)
+
+    def get_details(self, entry, config):
+        self.get_details_base(entry, config, self.build_selector())
+
+    def handle_join_date(self, value):
+        return parse(value, dayfirst=True).date()
--- ptsites/sites/hd-space.py
+++ ptsites/sites/hd-space.py
@@ -1,68 +1,64 @@
-from ..schema.site_base import SiteBase, Work, SignState
+from ..schema.xbtit import XBTIT
+from ..utils.net_utils import NetUtils
 
 
-class MainClass(SiteBase):
+class MainClass(XBTIT):
     URL = 'https://hd-space.org/'
+    SUCCEED_REGEX = 'Welcome back .*?</span> '
     USER_CLASSES = {
         'uploaded': [2199023255552],
         'share_ratio': [4.25]
     }
 
-    @classmethod
-    def build_workflow(cls):
-        return [
-            Work(
-                url='/',
-                method='get',
-                succeed_regex='Welcome back .*</span> ',
-                fail_regex=None,
-                check_state=('final', SignState.SUCCEED),
-                is_base_content=True
-            )
-        ]
-
-    def get_message(self, entry, config):
-        self.get_torrentleech_message(entry, config)
-
-    def get_details(self, entry, config):
-        self.get_details_base(entry, config, self.build_selector())
+    def __init__(self):
+        super().__init__()
 
     def build_selector(self):
-        selector = {
+        selector = super(MainClass, self).build_selector()
+        NetUtils.dict_merge(selector, {
+            'user_id': 'index.php\\?page=usercp&amp;uid=(\\d+)',
             'detail_sources': {
                 'default': {
+                    'link': '/index.php?page=usercp&uid={}',
                     'elements': {
-                        'bar': 'table.lista table.lista'
+                        'bar': 'table.lista table.lista',
+                        'table': 'body > div:nth-child(2) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table:nth-child(9) > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr > td:nth-child(4) > table'
                     }
                 }
             },
             'details': {
                 'uploaded': {
-                    'regex': 'UP:.+?([\\d.]+ [ZEPTGMK]B)'
+                    'regex': 'UP: ([\\d.]+ [ZEPTGMK]B)'
                 },
                 'downloaded': {
-                    'regex': 'DL:.+?([\\d.]+ [ZEPTGMK]B)'
+                    'regex': 'DL:  ([\\d.]+ [ZEPTGMK]B)'
                 },
                 'share_ratio': {
-                    'regex': 'Ratio:.+?(---|[\\d.]+)',
+                    'regex': 'Ratio: (---|[\\d.]+)',
                     'handle': self.handle_inf
+
                 },
                 'points': {
-                    'regex': 'Bonus:.+?(---|[\\d,.]+)',
+                    'regex': 'Bonus: (---|[\\d,.]+)',
                     'handle': self.handle_inf
+                },
+                'join_date': {
+                    'regex': 'Joined on.{5}(.*?\\d{4})',
+                    'handle': self.handle_join_date
                 },
                 'seeding': None,
                 'leeching': None,
                 'hr': None
             }
-        }
+
+        })
         return selector
 
-    def get_torrentleech_message(self, entry, config, messages_url='/messages.php'):
-        entry['result'] += '(TODO: Message)'
+    def get_message(self, entry, config):
+        self.get_XBTIT_message(entry, config,
+                               MESSAGES_URL_REGEX='index.php\\?page=usercp&amp;uid=\\d+&amp;do=pm&amp;action=list')
 
     def handle_inf(self, value):
         if value == '---':
-            return '0'
-        else:
-            return value
+            value = 0
+        return value
