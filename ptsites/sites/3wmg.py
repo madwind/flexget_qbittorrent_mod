09d37f20969a5252867bf784d45ea2c837d22b76
IvonWei <12468675@qq.com>
2021-06-12 09:52:59 +0800

    3wmg

---
+++ ptsites/sites/3wmg.py
@@ -0,0 +1,27 @@
+from ..schema.nexusphp import Attendance
+from ..utils.net_utils import NetUtils
+
+
+class MainClass(Attendance):
+    URL = 'https://www.3wmg.com/)'
+    USER_CLASSES = {
+        'downloaded': [1099511627776, 16492674416640],
+        'share_ratio': [3.05, 4.55],
+        'days': [280, 700]
+    }
+
+    def build_selector(self):
+        selector = super(MainClass, self).build_selector()
+        NetUtils.dict_merge(selector, {
+            'detail_sources': {
+                'default': {
+                    'do_not_strip': True
+                }
+            },
+            'details': {
+                'share_ratio': {
+                    'regex': '分享率：(無限|[\\d.]+)',
+                },
+            }
+        })
+        return selector
