862a746869168d189ac17ea243fe1ff912c164bb
IvonWei <12468675@qq.com>
2021-06-12 09:33:29 +0800

    USER_CLASSES

--- ptsites/sites/hd-torrents.py
+++ ptsites/sites/hd-torrents.py
@@ -5,7 +5,6 @@ class MainClass(XBTIT):
     URL = 'https://hd-torrents.org/'
     SUCCEED_REGEX = 'Welcome back, .+?!'
     USER_CLASSES = {
-        'uploaded': [8796093022208],
-        'share_ratio': [5.5],
-        'days': [70]
+        'uploaded': [1099511627776],
+        'share_ratio': [4]
     }
