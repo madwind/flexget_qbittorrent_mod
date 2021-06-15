ddaa1fc77ad4983c2539b1254998a04707c86001
IvonWei <12468675@qq.com>
2021-06-15 23:02:01 +0800

    hr

--- ptsites/sites/chdbits.py
+++ ptsites/sites/chdbits.py
@@ -1,10 +1,10 @@
-from ..schema.nexusphp import Bakatest
+from ..schema.nexusphp import BakatestHR
 
 
-class MainClass(Bakatest):
+class MainClass(BakatestHR):
     URL = 'https://chdbits.co/'
     USER_CLASSES = {
-        'downloaded': [3298534883328,4398046511104],
+        'downloaded': [3298534883328, 4398046511104],
         'share_ratio': [8, 10],
         'points': [3500000, 5000000],
         'days': [280, 364]
