5f1d4c9ed2766206e031208bf3084627ae735f1e
IvonWei <12468675@qq.com>
2021-09-16 11:41:43 +0800

    add itzmx

---
+++ ptsites/sites/itzmx.py
@@ -0,0 +1,10 @@
+from ..schema.nexusphp import Visit
+
+
+class MainClass(Visit):
+    URL = 'https://pt.itzmx.com/'
+    USER_CLASSES = {
+        'downloaded': [2147483648000, 1759218604442],
+        'share_ratio': [4, 6],
+        'days': [70, 1092]
+    }
