cd007a302390f1906cceaf2a19364f3bf03429e5
IvonWei <12468675@qq.com>
2021-09-16 11:37:55 +0800

    update url

--- ptsites/sites/byr.py
+++ ptsites/sites/byr.py
@@ -1,10 +1,9 @@
 from ..schema.nexusphp import Visit
 from ..schema.site_base import SignState, Work
-from ..utils.net_utils import NetUtils
 
 
 class MainClass(Visit):
-    URL = 'https://bt.byr.cn/'
+    URL = 'https://byr.pt/'
     USER_CLASSES = {
         'uploaded': [4398046511104, 140737488355328],
         'share_ratio': [3.05, 4.55],
