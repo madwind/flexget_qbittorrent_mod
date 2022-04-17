0a60b0422e1f443b89058fafa256b2caf95198d1
vivodi <103735539+vivodi@users.noreply.github.com>
2022-04-18 04:06:30 +0800

    fix: invalid SUCCEED_REGEX for exoticaz

--- ptsites/sites/exoticaz.py
+++ ptsites/sites/exoticaz.py
@@ -3,4 +3,4 @@ from ..schema.avistaz import AvistaZ
 
 class MainClass(AvistaZ):
     URL = 'https://exoticaz.to/'
-    SUCCEED_REGEX = '<h1 class="card-header h4">ExoticaZ Beta 1.0</h1>'
+    SUCCEED_REGEX = 'Logout'
