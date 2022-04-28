014b14dba9a15f9588a801ccd2443067eacf5d07
vivodi <103735539+vivodi@users.noreply.github.com>
2022-04-28 21:18:32 +0800

    Change the login method of hdpost to log in with a password instead of a cookie to prevent the login from expiring quickly.

--- ptsites/sites/hdpost.py
+++ ptsites/sites/hdpost.py
@@ -1,16 +1,35 @@
+import re
 from urllib.parse import urljoin
 
-from ..schema.site_base import SignState, Work
+from ..schema.site_base import SignState, Work, NetworkState
 from ..schema.unit3d import Unit3D
 
 
 class MainClass(Unit3D):
-    URL = 'https://pt.hdpost.top/'
-
+    URL = 'https://pt.hdpost.top'
     USER_CLASSES = {
         'uploaded': [10995116277760],
         'days': [365]
     }
+
+    @classmethod
+    def build_sign_in_schema(cls):
+        return {
+            cls.get_module_name(): {
+                'type': 'object',
+                'properties': {
+                    'login': {
+                        'type': 'object',
+                        'properties': {
+                            'username': {'type': 'string'},
+                            'password': {'type': 'string'}
+                        },
+                        'additionalProperties': False
+                    }
+                },
+                'additionalProperties': False
+            }
+        }
 
     @classmethod
     def build_reseed_schema(cls):
@@ -33,14 +52,44 @@ class MainClass(Unit3D):
     def build_workflow(self, entry, config):
         return [
             Work(
-                url='/',
+                url='/login',
                 method='get',
+                check_state=('network', NetworkState.SUCCEED),
+            ),
+            Work(
+                url='/login',
+                method='password',
                 succeed_regex='<title>HDPOST - 欢迎来到普斯特</title>',
                 fail_regex=None,
                 check_state=('final', SignState.SUCCEED),
-                is_base_content=True
+                is_base_content=True,
+                response_urls=[''],
+                token_regex=r'(?<=name="_token" value=").+?(?=")',
+                captcha_regex=r'(?<=name="_captcha" value=").+?(?=")',
             )
         ]
+
+    def sign_in_by_password(self, entry, config, work, last_content):
+        login = entry['site_config'].get('login')
+        if not login:
+            entry.fail_with_prefix('Login data not found!')
+            return
+        r = re.compile(r'name="(?P<name>.+?)" value="(?P<value>.+?)" />\s*<button type="submit"')
+        m = re.search(r, last_content)
+        data = {
+            '_token': re.search(work.token_regex, last_content).group(),
+            'username': login['username'],
+            'password': login['password'],
+            'remember': 'on',
+            '_captcha': re.search(work.captcha_regex, last_content).group(),
+            '_username': '',
+            m.group('name'): m.group('value'),
+        }
+        login_response = self._request(entry, 'post', work.url, data=data)
+        login_network_state = self.check_network_state(entry, work, login_response)
+        if login_network_state != NetworkState.SUCCEED:
+            return
+        return login_response
 
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
