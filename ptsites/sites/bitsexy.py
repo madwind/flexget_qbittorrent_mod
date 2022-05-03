da711c19b5d506b8466316cbb3d95d5cd78cc550
vivodi <103735539+vivodi@users.noreply.github.com>
2022-05-03 14:00:00 +0800

    Add bitsexy.org

---
+++ ptsites/sites/bitsexy.py
@@ -0,0 +1,89 @@
+import re
+
+from dateutil.parser import parse
+
+from ..schema.site_base import SiteBase, Work, SignState
+
+
+def handle_join_date(value):
+    return parse(value).date()
+
+
+def build_selector():
+    return {
+        'user_id': fr'''(?x)(?<= {re.escape('userdetails.php?id=')})
+                            (. +?)
+                            (?= ')''',
+        'detail_sources': {
+            'default': {
+                'link': '/userdetails.php?id={}',
+                'elements': {
+                    'stats': '#wrapper > div.mainheader > div > div.statusbar > div:nth-child(2) > div:nth-child(4)',
+                    'table': '#maincolumn > div.cblock-content > div > table:nth-child(4)',
+                }
+            }
+        },
+        'details': {
+            'uploaded': {
+                'regex': r'''(?x)(?<= Uploaded)
+                                ([\d.] +
+                                \ 
+                                [ZEPTGMKk] ? B)'''
+            },
+            'downloaded': {
+                'regex': r'''(?x)(?<= Downloaded)
+                                ([\d.] +
+                                \ 
+                                [ZEPTGMKk] ? B)'''
+            },
+            'share_ratio': {
+                'regex': r'''(?x)(?<= Ratio )
+                                ([\d,.] +)'''
+            },
+            'points': {
+                'regex': r'''(?x)(?<= Karma\ points)
+                                ([\d,.] +)'''
+            },
+            'join_date': {
+                'regex': r'''(?x)(?<= Join date)
+                                ((\w + \ ) {2}
+                                \w +)''',
+                'handle': handle_join_date
+            },
+            'seeding': {
+                'regex': r'''(?x)(?<= Seeding:\ )
+                                ([\d,] +)'''
+            },
+            'leeching': {
+                'regex': r'''(?x)(?<= Leeching:\ )
+                                ([\d,] +)'''
+            },
+            'hr': None,
+        }
+    }
+
+
+class MainClass(SiteBase):
+    URL = 'https://bitsexy.org/'
+    USER_CLASSES = {
+        'uploaded': [536_870_912_000],
+        'share_ratio': [3.05],
+        'days': [28],
+    }
+
+    def build_workflow(self, entry, config):
+        return [
+            Work(
+                url='/',
+                method='get',
+                succeed_regex=r'Logout',
+                check_state=('final', SignState.SUCCEED),
+                is_base_content=True,
+            ),
+        ]
+
+    def get_message(self, entry, config):
+        entry['result'] += '(TODO: Message)'  # TODO: Feature not implemented yet
+
+    def get_details(self, entry, config):
+        self.get_details_base(entry, config, build_selector())
