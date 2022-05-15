0aa5e5931e0fbb1d189170f6de9085a06aae600a
madwind <madwind.cn@gmail.com>
2022-05-15 10:42:24 +0800

    update build_selector

--- ptsites/sites/aither.py
+++ ptsites/sites/aither.py
@@ -1,5 +1,6 @@
 from ..schema.site_base import SignState, Work
 from ..schema.unit3d import Unit3D
+from ..utils.net_utils import NetUtils
 
 
 class MainClass(Unit3D):
@@ -23,4 +24,32 @@ class MainClass(Unit3D):
 
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
+        NetUtils.dict_merge(selector, {
+            'user_id': '/users/(.*?)/',
+            'detail_sources': {
+                'default': {
+                    'do_not_strip': True,
+                    'elements': {
+                        'bar': 'ul.top-nav__ratio-bar',
+                        'header': '.header',
+                        'data_table': '.user-info'
+                    }
+                }
+            },
+            'details': {
+                'points': {
+                    'regex': 'title="My Bonus Points".*?</i>.+?(\\d[\\d,. ]*)',
+                },
+                'share_ratio': {
+                    'regex': 'title="Ratio".*?</i>.+?(\\d[\\d,. ]*)',
+                },
+                'join_date': {
+                    'regex': 'Registration date (.*?\\d{4})',
+                    'handle': self.handle_join_date
+                },
+                'hr': {
+                    'regex': 'Active Warnings.+?(\\d+)'
+                }
+            }
+        })
         return selector
