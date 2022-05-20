54d4ff7d8b45bf1989cd98ae518324a08e481896
vivodi <103735539+vivodi@users.noreply.github.com>
2022-05-20 08:56:24 +0800

    fix not clocking in for hares (#79)
    * fix not clocking in for hares
    
    * fix invalid toffee regex
    
    * optimize the workflow for hares

--- ptsites/sites/hares.py
+++ ptsites/sites/hares.py
@@ -14,16 +14,26 @@ class MainClass(Attendance):
     def build_workflow(self, entry, config):
         return [
             Work(
-                url='/attendance.php',
+                url='/',
                 method='get',
+                succeed_regex='已签到',
+                check_state=('sign_in', SignState.NO_SIGN_IN),
+                is_base_content=True,
+            ),
+            Work(
+                url='/attendance.php?action=sign',
+                method='punch_in',
                 succeed_regex=[
-                    '这是您的第 \\d+ 次签到，已连续签到 \\d+ 天，本次签到获得 \\d+ 个奶糖。',
-                    '已签到'
+                    '签到成功',
+                    '您今天已经签到过了'
                 ],
                 check_state=('final', SignState.SUCCEED),
-                is_base_content=True
-            )
+            ),
         ]
+
+    def sign_in_by_punch_in(self, entry, config, work, last_content):
+        entry['headers']['accept'] = 'application/json'
+        return self._request(entry, 'get', work.url)
 
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
@@ -40,7 +50,7 @@ class MainClass(Attendance):
             },
             'details': {
                 'points': {
-                    'regex': '奶糖.*?([\\d,.]+)',
+                    'regex': '奶糖.(?:>.*?){4}([\\d,.]+)',
                     'handle': self.handle_points
                 },
                 'seeding': {
