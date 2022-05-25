560b84d589f0b65e6b2d1621a1be539bb6d897b8
madwind <madwind.cn@gmail.com>
2022-05-25 21:23:12 +0800

    add gainbound

---
+++ ptsites/sites/gainbound.py
@@ -0,0 +1,24 @@
+from ..schema.nexusphp import Attendance
+from ..utils.net_utils import dict_merge
+
+
+class MainClass(Attendance):
+    URL = 'https://gainbound.net/'
+    USER_CLASSES = {
+        'uploaded': [805306368000, 3298534883328],
+        'share_ratio': [3.05, 4.55],
+        'points': [400000, 1000000],
+        'days': [280, 700]
+    }
+
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': r'做种积分: ([\d.]+)'
+                }
+            }
+        })
+        return selector
