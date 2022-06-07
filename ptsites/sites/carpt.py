5cfb72bff4299eb210719e77cdbdd5cfa296534f
madwind <madwind.cn@gmail.com>
2022-06-07 10:54:02 +0800

    忽略 'H&R\(ID: \d+\) 已达标' 消息

--- ptsites/sites/carpt.py
+++ ptsites/sites/carpt.py
@@ -1,12 +1,17 @@
 from typing import Final
 
+from ..base.entry import SignInEntry
 from ..schema.nexusphp import AttendanceHR
 
 
 class MainClass(AttendanceHR):
     URL: Final = 'https://carpt.net/'
+    IGNORE_TITLE = r'H&R\(ID: \d+\) 已达标'
     USER_CLASSES: Final = {
         'downloaded': [805306368000, 3298534883328],
         'share_ratio': [6, 9],
         'days': [280, 700]
     }
+
+    def get_messages(self, entry: SignInEntry, config: dict) -> None:
+        self.get_nexusphp_messages(entry, config, ignore_title=self.IGNORE_TITLE)
