e841f71e6bee69d46730fb37898d153006c1a048
madwind <madwind.cn@gmail.com>
2022-07-06 17:36:03 +0800

    支持 cookie 登录, 调整 secret_key 配置位置

--- ptsites/sites/m-team.py
+++ ptsites/sites/m-team.py
@@ -16,6 +16,8 @@ from ..utils.net_utils import get_module_name
 
 class MainClass(NexusPHP):
     URL: Final = 'https://kp.m-team.cc/'
+    VERIFY_URL: Final = '/verify.php?returnto='
+    SUCCEED_REGEX = '歡迎回來'
     USER_CLASSES: Final = {
         'downloaded': [2147483648000, 3221225472000],
         'share_ratio': [7, 9],
@@ -28,12 +30,13 @@ class MainClass(NexusPHP):
             get_module_name(cls): {
                 'type': 'object',
                 'properties': {
+                    'cookie': {'type': 'string'},
+                    'secret_key': {'type': 'string'},
                     'login': {
                         'type': 'object',
                         'properties': {
                             'username': {'type': 'string'},
-                            'password': {'type': 'string'},
-                            'secret_key': {'type': 'string'}
+                            'password': {'type': 'string'}
                         },
                         'additionalProperties': False
                     }
@@ -46,48 +49,68 @@ class MainClass(NexusPHP):
         return [
             Work(
                 url='/takelogin.php',
-                method=self.sign_in_by_verify,
-                succeed_regex=['歡迎回來'],
+                method=self.sign_in_by_login,
+                succeed_regex=[self.SUCCEED_REGEX],
                 assert_state=(check_final_state, SignState.SUCCEED),
                 response_urls=['/verify.php?returnto=', '/index.php'],
                 is_base_content=True,
-                verify_url='/verify.php?returnto=',
+            )
+        ]
+
+    def sign_in_build_workflow(self, entry: SignInEntry, config: dict) -> list[Work]:
+        return [
+            Work(
+                url='/',
+                method=self.sign_in_by_get,
+                succeed_regex=[self.SUCCEED_REGEX],
+                assert_state=(check_final_state, SignState.SUCCEED),
+                response_urls=['/verify.php?returnto=%2F', '/'],
+                is_base_content=True
             )
         ]
 
     def sign_in_build_login_data(self, login: dict, last_content: str) -> dict:
-        return {
-            'username': login['username'],
-            'password': login['password'],
-        }
+        return login
 
-    def sign_in_by_verify(self, entry: SignInEntry, config: dict, work: Work, last_content: str) -> Response | None:
-        if not (login := entry['site_config'].get('login')):
-            entry.fail_with_prefix('Login data not found!')
+    def sign_in_by_login(self, entry: SignInEntry, config: dict, work: Work, last_content: str) -> Response | None:
+        login_response = super().sign_in_by_login(entry, config, work, last_content)
+        if check_network_state(entry, work, login_response) != NetworkState.SUCCEED:
             return None
+        verify_response = self.check_verify(entry, login_response)
+        return verify_response
 
-        login_response = self.sign_in_by_login(entry, config, work, last_content)
-
-        login_network_state = check_network_state(entry, work, login_response)
-        if login_network_state != NetworkState.SUCCEED:
+    def sign_in_by_get(self,
+                       entry: SignInEntry,
+                       config: dict,
+                       work: Work,
+                       last_content: str = None,
+                       ) -> Response | None:
+        response = super().sign_in_by_get(entry, config, work, last_content)
+        if check_network_state(entry, work, response) != NetworkState.SUCCEED:
             return None
-        verify_url = urljoin(entry['url'], work.verify_url)
-        if login_response.url.startswith(verify_url):
-            content = net_utils.decode(login_response)
+        verify_response = self.check_verify(entry, response)
+        return verify_response
+
+    def check_verify(self, entry, response: Response) -> Response | None:
+        verify_url = urljoin(entry['url'], MainClass.VERIFY_URL)
+        if response.url.startswith(verify_url):
+            if not (secret_key := entry['site_config'].get('secret_key')):
+                entry.fail_with_prefix('Attempts text not found!  with google_auth')
+                return response
+            content = net_utils.decode(response)
             attempts = re.search('您還有(\\d+)次嘗試機會，否則該IP將被禁止訪問。', content)
             if attempts:
                 times = attempts.group(1)
                 if times == '30':
-                    google_code = google_auth.calc(login['secret_key'])
+                    google_code = google_auth.calc(secret_key)
                     data = {
                         'otp': (None, google_code)
                     }
                     return self.request(entry, 'post', verify_url, files=data)
-                else:
-                    entry.fail_with_prefix('{} with google_auth'.format(attempts.group()))
+                entry.fail_with_prefix(f'{attempts.group()} with google_auth')
             else:
                 entry.fail_with_prefix('Attempts text not found!  with google_auth')
-        return login_response
+        return response
 
     def get_messages(self, entry: SignInEntry, config: dict) -> None:
         self.get_nexusphp_messages(entry, config)
