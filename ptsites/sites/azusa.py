5a63c6a98d887b8fa283d7ee512b3743625a7d17
madwind <madwind.cn@gmail.com>
2022-08-14 20:22:52 +0800

    azusa

---
+++ ptsites/sites/azusa.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+from ..schema.nexusphp import Attendance
+
+
+class MainClass(Attendance):
+    URL: Final = 'https://azusa.ru/'
+    USER_CLASSES: Final = {
+        'downloaded': [805306368000, 3298534883328],
+        'share_ratio': [3.05, 4.55],
+        'days': [280, 700]
+    }
