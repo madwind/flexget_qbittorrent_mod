3fb5cc17bb1fc8aef6ad8a9a0aea3fa19bcefa8b
madwind <madwind.cn@gmail.com>
2022-08-25 08:22:20 +0800

    USER_CLASSES

--- ptsites/sites/hdsky.py
+++ ptsites/sites/hdsky.py
@@ -29,9 +29,9 @@ class MainClass(NexusPHP):
     TORRENT_PAGE_URL: Final = '/details.php?id={torrent_id}&hit=1'
     DOWNLOAD_URL_REGEX: Final = '/download\\.php\\?id=\\d+&passkey=.*?(?=")'
     USER_CLASSES: Final = {
-        'downloaded': [10995116277760],
-        'share_ratio': [5.5],
-        'days': [455]
+        'downloaded': [8796093022208, 10995116277760],
+        'share_ratio': [5, 5.5],
+        'days': [315, 455]
     }
 
     @classmethod
