c4829eb20b7aed3acd2fd3803422db2894f7d183
madwind <madwind.cn@gmail.com>
2022-10-01 12:33:21 +0800

    table selector

--- ptsites/sites/ultrahd.py
+++ ptsites/sites/ultrahd.py
@@ -1,6 +1,7 @@
 from typing import Final
 
 from ..schema.nexusphp import Attendance
+from ..utils import net_utils
 
 
 class MainClass(Attendance):
@@ -10,3 +11,17 @@ class MainClass(Attendance):
         'share_ratio': [5, 6.5],
         'days': [175, 280]
     }
+
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'detail_sources': {
+                'default': {
+                    'elements': {
+                        'table': '#outer > font > table:last-of-type',
+                    }
+                }
+            }
+        })
+        return selector
