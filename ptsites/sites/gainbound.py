b61332b712f40cb3d40129ebe6080ea9173b3c51
madwind <madwind.cn@gmail.com>
2022-10-04 09:28:02 +0800

    USER_CLASSES

--- ptsites/sites/gainbound.py
+++ ptsites/sites/gainbound.py
@@ -7,7 +7,7 @@ from ..utils.net_utils import dict_merge, get_module_name
 class MainClass(Attendance):
     URL: Final = 'https://gainbound.net/'
     USER_CLASSES: Final = {
-        'uploaded': [805306368000, 3298534883328],
+        'downloaded': [805306368000, 3298534883328],
         'share_ratio': [3.05, 4.55],
         'points': [400000, 1000000],
         'days': [280, 700]
