01127cac302fd4114c03423fde8709be31f0df29
madwind <madwind.cn@gmail.com>
2022-10-21 09:57:56 +0800

    icc2022

---
+++ ptsites/sites/icc2022.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Final
+
+from ..schema.nexusphp import AttendanceHR
+from ..utils.value_handler import size
+
+
+class MainClass(AttendanceHR):
+    URL: Final = 'https://www.icc2022.com/'
+    USER_CLASSES: Final = {
+        'downloaded': [size(750, 'GiB'), size(3, 'TiB')],
+        'share_ratio': [3.05, 4.45],
+        'points': [400000, 1000000],
+        'days': [280, 700]
+    }
