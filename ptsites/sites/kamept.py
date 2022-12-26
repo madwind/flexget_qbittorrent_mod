df1db2410fb95559cc5bf7226f5bfd389a8683ed
madwind <madwind.cn@gmail.com>
2022-12-27 00:23:00 +0800

    add kamept fix ultrahd

---
+++ ptsites/sites/kamept.py
@@ -0,0 +1,27 @@
+from typing import Final
+
+from ..schema.nexusphp import Attendance
+from ..utils import net_utils
+from ..utils.value_handler import size
+
+
+class MainClass(Attendance):
+    URL: Final = 'https://kamept.com/'
+    USER_CLASSES: Final = {
+        'downloaded': [size(750, 'GiB'), size(3, 'TiB')],
+        'share_ratio': [3.05, 4.55],
+        'points': [400000, 1000000],
+        'days': [280, 700]
+    }
+
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': (r'(做种积分).*?([\d,.]+)', 2)
+                },
+            }
+        })
+        return selector
--- ptsites/sites/ultrahd.py
+++ ptsites/sites/ultrahd.py
@@ -1,7 +1,6 @@
 from typing import Final
 
 from ..schema.nexusphp import Attendance
-from ..utils import net_utils
 
 
 class MainClass(Attendance):
@@ -11,17 +10,3 @@ class MainClass(Attendance):
         'share_ratio': [5, 6.5],
         'days': [175, 280]
     }
-
-    @property
-    def details_selector(self) -> dict:
-        selector = super().details_selector
-        net_utils.dict_merge(selector, {
-            'detail_sources': {
-                'default': {
-                    'elements': {
-                        'table': '#outer > font > table:last-of-type',
-                    }
-                }
-            }
-        })
-        return selector
