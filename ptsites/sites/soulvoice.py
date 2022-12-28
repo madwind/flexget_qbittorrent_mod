6b61e0828eec38d85738c81abbed21f460d7ddb7
madwind <madwind.cn@gmail.com>
2022-12-28 09:53:15 +0800

    delay 2s

--- ptsites/sites/soulvoice.py
+++ ptsites/sites/soulvoice.py
@@ -1,5 +1,9 @@
+from time import sleep
 from typing import Final
 
+from requests import Response
+
+from ..base.entry import SignInEntry
 from ..base.reseed import ReseedPasskey
 from ..schema.nexusphp import AttendanceHR
 
@@ -11,3 +15,12 @@ class MainClass(AttendanceHR, ReseedPasskey):
         'share_ratio': [3.05, 4.55],
         'days': [280, 700]
     }
+
+    def request(self,
+                entry: SignInEntry,
+                method: str,
+                url: str,
+                **kwargs,
+                ) -> Response | None:
+        sleep(2)
+        return super().request(entry, method, url, **kwargs)
