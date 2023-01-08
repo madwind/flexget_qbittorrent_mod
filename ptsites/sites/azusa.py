cb1c61be95c8911a2b39ad29e456077a2d9c2fc4
madwind <madwind.cn@gmail.com>
2023-01-08 21:33:55 +0800

    azusa url

--- ptsites/sites/azusa.py
+++ ptsites/sites/azusa.py
@@ -4,7 +4,7 @@ from ..schema.nexusphp import Attendance
 
 
 class MainClass(Attendance):
-    URL: Final = 'https://azusa.ru/'
+    URL: Final = 'https://azusa.wiki/'
     USER_CLASSES: Final = {
         'downloaded': [805306368000, 3298534883328],
         'share_ratio': [3.05, 4.55],
