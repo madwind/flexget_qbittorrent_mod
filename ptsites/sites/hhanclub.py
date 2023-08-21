a8e47b684baa66cb80866d9171bae98339ac9f76
madwind <madwind.cn@gmail.com>
2023-08-21 16:32:34 +0800

    hhanclub

--- ptsites/sites/hhanclub.py
+++ ptsites/sites/hhanclub.py
@@ -1,9 +1,12 @@
 from typing import Final
 
+from ..base.entry import SignInEntry
 from ..base.reseed import ReseedPasskey
+from ..base.sign_in import check_final_state, SignState
+from ..base.work import Work
 from ..schema.nexusphp import AttendanceHR
 from ..utils import net_utils
-from ..utils.value_handler import size
+from ..utils.value_handler import size, handle_infinite
 
 
 class MainClass(AttendanceHR, ReseedPasskey):
@@ -14,14 +17,57 @@ class MainClass(AttendanceHR, ReseedPasskey):
         'days': [280, 700]
     }
 
+
     @property
     def details_selector(self) -> dict:
-        selector = super().details_selector
-        net_utils.dict_merge(selector, {
+        return {
+            'user_id': r'userdetails\.php\?id=(\d+)',
+            'detail_sources': {
+                'default': {
+                    'link': '/userdetails.php?id={}',
+                    'elements': {
+                        'panel': '#user-info-panel',
+                        'table': '#mainContent'
+                    }
+                }
+            },
             'details': {
+                'uploaded': {
+                    'regex': r'上传量.+?([\d.]+ ?[ZEPTGMK]?i?B)'
+                },
+                'downloaded': {
+                    'regex': r'下载量.+?([\d.]+ ?[ZEPTGMK]?i?B)'
+                },
+                'share_ratio': {
+                    'regex': r'分享率.*?(---|∞|Inf\.|无限|無限|[\d,.]+)',
+                    'handle': handle_infinite
+                },
                 'points': {
-                    'regex': '憨豆.*?([\\d,.]+)'
+                    'regex': r'憨豆：.*?([\d,.]+)'
+                },
+                'join_date': {
+                    'regex': r'加入日期.*?(\d{4}-\d{2}-\d{2})',
+                },
+                'seeding': {
+                    'regex': (r'勋章.*?([\d,.]+).*?([\d,.]+)', 2)
+                },
+                'leeching': {
+                    'regex': (r'勋章.*?([\d,.]+).*?([\d,.]+).*?([\d,.]+).*?([\d,.]+)', 4)
+                },
+                'hr': {
+                    'regex': r'H&R.*?(\d+)'
                 }
             }
-        })
-        return selector
+        }
+
+
+    def sign_in_build_workflow(self, entry: SignInEntry, config: dict) -> list[Work]:
+        return [
+            Work(
+                url='/attendance.php',
+                method=self.sign_in_by_get,
+                succeed_regex=[r'这是您的第\d+次签到，已连续签到\d+天，本次签到获得\d+个憨豆。'],
+                assert_state=(check_final_state, SignState.SUCCEED),
+                is_base_content=True
+            )
+        ]
