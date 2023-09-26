17b45a3f9663bcb89805705b1938baad6c0b4db6
madwind <madwind.cn@gmail.com>
2023-09-26 08:10:43 +0800

    fix: hddolby

--- ptsites/sites/hddolby.py
+++ ptsites/sites/hddolby.py
@@ -2,12 +2,27 @@ from typing import Final
 
 from ..base.reseed import ReseedCookie
 from ..schema.nexusphp import AttendanceHR
+from ..utils import net_utils
+from ..utils.value_handler import size
 
 
 class MainClass(AttendanceHR, ReseedCookie):
     URL: Final = 'https://www.hddolby.com/'
     USER_CLASSES: Final = {
-        'downloaded': [1099511627776, 8796093022208],
-        'share_ratio': [4, 5.5],
-        'days': [112, 336]
+        'downloaded': [size(2,'TiB'), size(8,'TiB')],
+        'share_ratio': [4.5, 5.5],
+        'points': [470400, 1128960],
+        'days': [140, 336]
     }
+
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': '做种积分.*?([\\d,.]+)'
+                }
+            }
+        })
+        return selector
