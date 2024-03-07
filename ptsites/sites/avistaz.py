2e9961c89237194bf65c2087d928ccc80ce8dd4f
madwind <madwind.cn@gmail.com>
2024-03-07 19:36:52 +0800

    fix SUCCEED_REGEX

--- ptsites/sites/avistaz.py
+++ ptsites/sites/avistaz.py
@@ -5,4 +5,4 @@ from ..schema.avistaz import AvistaZ
 
 class MainClass(AvistaZ):
     URL: Final = 'https://avistaz.to/'
-    SUCCEED_REGEX: Final = '<title>AvistaZ Beta 3.0</title>'
+    SUCCEED_REGEX: Final = 'Torrents'
