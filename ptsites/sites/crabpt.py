4b7e3c1e09bdbb52794146e0de04e562bf26dc73
madwind <madwind.cn@gmail.com>
2024-06-11 12:00:26 +0800

    add crabpt

---
+++ ptsites/sites/crabpt.py
@@ -0,0 +1,28 @@
+from typing import Final
+
+from ..base.reseed import ReseedPasskey
+from ..schema.nexusphp import Attendance
+from ..utils import net_utils
+from ..utils.value_handler import size
+
+
+class MainClass(Attendance, ReseedPasskey):
+    URL: Final = 'https://crabpt.vip/'
+    USER_CLASSES: Final = {
+        'downloaded': [size(750, 'GiB'), size(3, 'TiB')],
+        'share_ratio': [3.05, 4.55],
+        'points': [400000, 1000000],
+        'days': [280, 700]
+    }
+
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'details': {
+                'points': {
+                    'regex': (r'(蟹币值).*?([\d,.]+)', 2)
+                }
+            }
+        })
+        return selector
