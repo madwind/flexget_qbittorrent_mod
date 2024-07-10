0390aa80437209ec78f4226f3a3cfd6a37f20f31
Bright-W <coolup9@gmail.com>
2024-07-11 06:28:14 +0800

    fix hdt (#159)
    * fix hdt
    
    * fix hdt
    
    * fix hdt

--- ptsites/sites/hd-torrents.py
+++ ptsites/sites/hd-torrents.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from ..base.reseed import ReseedPasskey
 from ..schema.xbtit import XBTIT
+from ..utils import net_utils
 
 
 class MainClass(XBTIT, ReseedPasskey):
@@ -11,3 +12,17 @@ class MainClass(XBTIT, ReseedPasskey):
         'uploaded': [1099511627776],
         'share_ratio': [4]
     }
+    
+    @property
+    def details_selector(self) -> dict:
+        selector = super().details_selector
+        net_utils.dict_merge(selector, {
+            'detail_sources': {
+                'default': {
+                    'elements': {
+                        'bar': 'body > div.mainmenu > table:nth-child(4)',
+                    }
+                }
+            }
+        })
+        return selector
