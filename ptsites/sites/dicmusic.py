f1cda6b57cd0dbcc116dc2f3c3e2e08cfa2f8843
IvonWei <madwind.cn@gmail.com>
2024-10-28 08:30:19 +0800

    Revert "fix dicmusic.com (#162)"
    This reverts commit 7a52c002df699dcfeaa9042775f48e595f2f9827.

--- ptsites/sites/dicmusic.py
+++ ptsites/sites/dicmusic.py
@@ -24,26 +24,6 @@ class MainClass(Gazelle, Reseed):
     }
 
     @classmethod
-    def sign_in_build_schema(cls):
-        return {
-            get_module_name(cls): {
-                'type': 'object',
-                'properties': {
-                    'cookie': {'type': 'string'},
-                    'login': {
-                        'type': 'object',
-                        'properties': {
-                            'username': {'type': 'string'},
-                            'password': {'type': 'string'}
-                        },
-                        'additionalProperties': False
-                    }
-                },
-                'additionalProperties': False
-            }
-        }
-
-    @classmethod
     def reseed_build_schema(cls):
         return {
             get_module_name(cls): {
@@ -60,22 +40,13 @@ class MainClass(Gazelle, Reseed):
     def sign_in_build_workflow(self, entry: SignInEntry, config: dict) -> list[Work]:
         return [
             Work(
-                url='/login.php',
-                method=self.sign_in_by_login,
+                url='/',
+                method=self.sign_in_by_get,
                 succeed_regex=['积分 \\(.*?\\)'],
                 assert_state=(check_final_state, SignState.SUCCEED),
-                is_base_content=True,
-                response_urls=['/index.php']
+                is_base_content=True
             )
         ]
-
-    def sign_in_build_login_data(self, login: dict, last_content: str) -> dict:
-        return {
-            'username': login['username'],
-            'password': login['password'],
-            'keeplogged': 1,
-            'login': '登录',
-        }
 
     @property
     def details_selector(self) -> dict:
